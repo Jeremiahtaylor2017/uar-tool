18c7964a3ebc981ace03423e2d951cc158fe27cb
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-08-21 01:29:49 -0700

    styling

--- src/app/controls/page.tsx
+++ src/app/controls/page.tsx
@@ -5,11 +5,11 @@ type Props = {}
 
 const Controls = (props: Props) => {
     return (
-        <div className='m-4'>
+        <div className='m-4 h-full flex flex-col justify-end'>
             <div className='mb-16'>
                 <h1 className='text-6xl'>User Access Controls</h1>
             </div>
-            <div>
+            <div className='grow'>
                 <ControlTable />
             </div>
         </div>
