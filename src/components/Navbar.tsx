0444f0ed7d123f1049180debd62432a89090627e
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-08-21 01:30:14 -0700

    styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const Navbar = (props: Props) => {
     return (
-        <div className='border border-[#bbb] w-56'>
+        <div className='border border-[#bbb] w-56 sticky'>
             <div className='mt-16 pl-8 flex flex-col gap-4'>
                 <Link href="/access-reviews">Access Reviews</Link>
                 <Link href="/controls">Controls</Link>
