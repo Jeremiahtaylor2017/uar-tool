18f9bd8f2d55b603797f395b624e803f3775af00
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-08-20 18:14:00 -0700

    base styles and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <Navbar />
+          <div className="content-container">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
