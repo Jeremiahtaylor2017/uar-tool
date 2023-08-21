9697cf2aaaafebcecb760ca61af899dfe06c1c68
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-08-20 18:14:27 -0700

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <div className='border border-[#bbb] w-56'>
+            <div className='mt-16 pl-8 flex flex-col gap-4'>
+                <Link href="/access-reviews">Access Reviews</Link>
+                <Link href="/control">Control</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
