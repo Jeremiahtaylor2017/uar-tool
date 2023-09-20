c1349ca0f1e16b205da2a3228aacc4c962cad174
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-09-20 00:27:13 -0700

    leaked a secret whoopsy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,5 @@ export default {
     driver: "mysql2",
     dbCredentials: {
         connectionString: process.env.DATABASE_URL || 'mysql://user:pass@db.com'
-        // connectionString: 'mysql://e38g9vufujg6u0yojk7l:pscale_pw_ikT5xUYOIXzTKOIMMURN1BxmHfmtrqaNfMRqlGt1Yz9@aws.connect.psdb.cloud/uac-tool?ssl={"rejectUnauthorized":true}'
     }
 } satisfies Config;
